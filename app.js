b9f00ca749a1ccca0c4b1c715c26213125512996
Logan Spencer <lspencer@revmansolutions.com>
2018-05-15 10:26:10 -0500

    configure port to work with heroku

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var port = process.env.PORT || 8080;
+
 var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -31,6 +33,6 @@ app.post('/mail', function(req, res) {
     res.sendStatus(200);
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function() {
+  console.log('Example app listening on port ' + port);
 });
